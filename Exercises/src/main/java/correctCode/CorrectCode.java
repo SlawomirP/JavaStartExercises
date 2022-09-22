35df6f2e16d5ebfc67a02a9766b50c54970c60c9
slawo <slawekpettke@gmail.com>
2022-09-22 16:58:05 +0200

    ex2

---
+++ Exercises/src/main/java/correctCode/CorrectCode.java
@@ -0,0 +1,10 @@
+package correctCode;
+
+public class CorrectCode {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 4;
+        double div =  (double)x / y;
+        System.out.println(div);
+    }
+}
